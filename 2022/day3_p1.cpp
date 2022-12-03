c0655cbae73810764d3c4a4c8f3adafe6e2a3cab
Madhav Ram <madhav6ram@gmail.com>
2022-12-03 14:40:47 +0530

    Create day3_p1.cpp

---
+++ 2022/day3_p1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main() {
+	
+	ll sum=0;
+	
+	l1: ;
+	for(string s; getline(cin, s);) {
+	    string s1 = s.substr(0, s.length()/2);
+        string s2 = s.substr(s.length()/2);
+        for(int i=0; i<s1.length(); i++) {
+            for(int j=0; j<s1.length(); j++){
+                if(s[i]==s2[j]) {
+                    if(islower(s[i]))
+                        sum+=s[i]-'a'+1;
+                    else
+                        sum+=26+(s[i]-'A'+1);
+                    goto l1;
+                }
+            }
+        }
+    }
+    
+	cout << sum;
+    
+	return 0;
+}
